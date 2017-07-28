546154a2c05b091ceb6939a67af72b5845dfdabb
Zach Lowe <lowezake@gmail.com>
2017-07-28 11:47:27 -0600

    updated

--- js/app.js
+++ js/app.js
@@ -1,72 +1,60 @@
-$(document).ready(function(){
-  'use strict';
-  $.get("https://ombd-api.now.sh/"), function(data) {
-    movies.push(movie);
-    renderMovies();
-  }
+$(document).ready(function() {
+    $.get("https://omdb-api.now.sh/?s=reservoir%20dogs", function(data) {
+        renderMovies();
+        const movies = [];
+        const movie = {};
+        movie.push((data.Search[0]))
+        const renderMovies = function() {
+            $('#listings').empty();
+            for (const movie of movies) {
+                const $col = $('<div>').addClass('col s6');
+                const $card = $('<div>').addClass('card hoverable');
+                const $content = $('<div>').addClass('card-content center');
+                const $title = $('<h6>').addClass('card-title truncate');
+                $title.attr({
+                    'data-position': 'top',
+                    'data-tooltip': movie.title
+                });
+                $title.tooltip({ delay: 50 }).text(movie.title);
+                const $poster = $('<img>').addClass('poster');
+                $poster.attr({
+                    src: movie.poster,
+                    alt: `${movie.poster} Poster`
+                });
+                $content.append($title, $poster);
+                $card.append($content);
+                const $action = $('<div>').addClass('card-action center');
+                const $plot = $('<a>');
+                $plot.addClass('waves-effect waves-light btn modal-trigger');
+                $plot.attr('href', `#${movie.id}`);
+                $plot.text('Plot Synopsis');
+                $action.append($plot);
+                $card.append($action);
+                const $modal = $('<div>').addClass('modal').attr('id', movie.id);
+                const $modalContent = $('<div>').addClass('modal-content');
+                const $modalHeader = $('<h4>').text(movie.title);
+                const $movieYear = $('<h6>').text(`Released in ${movie.year}`);
+                const $modalText = $('<p>').text(movie.plot);
+                $modalContent.append($modalHeader, $movieYear, $modalText);
+                $modal.append($modalContent);
+                $col.append($card, $modal);
+                $('#listings').append($col);
+                $('.modal-trigger').leanModal();
+            }
+        };
+        $("#butt").click(function(x) {
+            if ($("#search").val().length == 0)
+                alert("Please enter input");
+            x.preventDefault()
+            $("#search").empty()
+        });
+    });
+})
 
-  var movies = [];
 
-  var movie = {};
 
-  var renderMovies = function() {
-    $('#listings').empty();
-
-    for (var movie of movies) {
-      const $col = $('<div>').addClass('col s6');
-      const $card = $('<div>').addClass('card hoverable');
-      const $content = $('<div>').addClass('card-content center');
-      const $title = $('<h6>').addClass('card-title truncate');
-
-      $title.attr({
-        'data-position': 'top',
-        'data-tooltip': movie.title
-      });
-
-      $title.tooltip({ delay: 50 }).text(movie.title);
-
-      const $poster = $('<img>').addClass('poster');
-
-      $poster.attr({
-        src: movie.poster,
-        alt: `${movie.poster} Poster`
-      });
-
-      $content.append($title, $poster);
-      $card.append($content);
-
-      const $action = $('<div>').addClass('card-action center');
-      const $plot = $('<a>');
-
-      $plot.addClass('waves-effect waves-light btn modal-trigger');
-      $plot.attr('href', `#${movie.id}`);
-      $plot.text('Plot Synopsis');
-
-      $action.append($plot);
-      $card.append($action);
-
-      const $modal = $('<div>').addClass('modal').attr('id', movie.id);
-      const $modalContent = $('<div>').addClass('modal-content');
-      const $modalHeader = $('<h4>').text(movie.title);
-      const $movieYear = $('<h6>').text(`Released in ${movie.year}`);
-      const $modalText = $('<p>').text(movie.plot);
-
-      $modalContent.append($modalHeader, $movieYear, $modalText);
-      $modal.append($modalContent);
-
-      $col.append($card, $modal);
-
-      $('#listings').append($col);
-
-      $('.modal-trigger').leanModal();
-    }
-  };
-
-$(".btn-large waves-effect waves-light").click(function(x){
-  x.preventDefault()
-  $("#search").empty()
-});
- // .fail(function() {
- //  alert(“The user input is blank”);
- //  }
-});
+// var title = data.Search[0].Title
+// var id = data.Search[0].imdbID
+// var poster = data.Search[0].Poster
+// var year = data.Search[0].Year
+//console.log(2, data.Search[0])
